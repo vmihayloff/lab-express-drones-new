0ae6e3d4458cc4f6ac95510a94569d98a4998e99
Manish Poduval <manishpoduval@hotmail.com>
2021-02-05 11:27:33 +0530

    removed old libraries and code

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 const express = require('express');
 const favicon = require('serve-favicon');
 const hbs = require('hbs');
@@ -9,10 +8,6 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(
-  `${app_name}:${path.basename(__filename).split('.')[0]}`
-);
 
 const app = express();
 
@@ -23,13 +18,15 @@ require('./configs/db.config');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 
 // Express View engine setup
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Read about this
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
